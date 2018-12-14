953bbd9e9662340a46289f3d46d89ab279cf1589
Minh Nguyen <tienminh.uet@gmail.com>
2018-12-14 10:43:55 +0700

    ok

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 # make sure the privious version is deleted and re-created
-rm -rf /var/www/php-provinces
-mkdir -vp /var/www/php-provinces
+#rm -rf /var/www/php-provinces
+#mkdir -vp /var/www/php-provinces
+
+echo 'before install...'
